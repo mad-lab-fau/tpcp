54c0990e2e4e6ed592793fabcb3ca3b2005ee90a
Arne Küderle <a.kuederle@gmail.com>
2021-11-03 17:51:50 +0100

    Bumped to alpha release

--- tpcp/__init__.py
+++ tpcp/__init__.py
@@ -1,6 +1,6 @@
 """tpcp - Tiny Pipelines for Complicated Problems."""
 from tpcp._base import BaseAlgorithm, BaseOptimize
 
-__version__ = "0.1.0"
+__version__ = "0.2.0-alpha.0"
 
 __all__ = ["BaseAlgorithm", "BaseOptimize"]
