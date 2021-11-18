92c4e65561dd77f71214a3d3889b73895e87ad03
Arne Küderle <a.kuederle@gmail.com>
2021-11-18 19:20:50 +0100

    Version bump

--- tpcp/__init__.py
+++ tpcp/__init__.py
@@ -2,7 +2,7 @@
 from tpcp._utils._general import clone, default
 from tpcp.base import BaseAlgorithm, BaseOptimize
 
-__version__ = "0.2.0-alpha.3"
+__version__ = "0.2.0"
 
 mdf = default
 __all__ = ["BaseAlgorithm", "BaseOptimize", "default", "mdf", "clone"]
