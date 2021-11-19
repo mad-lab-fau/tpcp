4f2411edef1c5664f4ee901d67bde5b76ac5cf20
Arne Küderle <a.kuederle@gmail.com>
2021-11-19 10:48:10 +0100

    bump version

--- tpcp/__init__.py
+++ tpcp/__init__.py
@@ -4,7 +4,7 @@ from tpcp._pipelines import OptimizablePipeline, SimplePipeline
 from tpcp._utils._general import clone, default
 from tpcp.base import BaseAlgorithm
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 
 mdf = default
