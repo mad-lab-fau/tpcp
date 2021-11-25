a9ad13113bf7f5f67d6018fa7861c5f22e17bcbf
Arne Küderle <a.kuederle@gmail.com>
2021-11-25 12:06:22 +0100

    Refactored safe_run so that it can also be applied to algorithms

--- tpcp/__init__.py
+++ tpcp/__init__.py
@@ -1,8 +1,9 @@
 """tpcp - Tiny Pipelines for Complicated Problems."""
 from tpcp._dataset import Dataset
 from tpcp._pipelines import OptimizablePipeline, SimplePipeline
-from tpcp._utils._general import clone, default
+from tpcp._utils._general import clone, default, safe_action
 from tpcp.base import BaseAlgorithm, BaseTpcpObject, OptimizableAlgorithm
+from tpcp.base import BaseAlgorithm, OptimizableAlgorithm
 
 __version__ = "0.3.1"
 
@@ -13,6 +14,7 @@ __all__ = [
     "default",
     "mdf",
     "clone",
+    "safe_action",
     "BaseAlgorithm",
     "OptimizableAlgorithm",
     "BaseTpcpObject",
