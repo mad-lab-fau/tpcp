d112beca75cba2e4812dae96568680fd259721aa
Arne Küderle <a.kuederle@gmail.com>
2021-11-26 09:24:07 +0100

    First simple tests for examples

---
+++ tests/test_examples/test_all_examples.py
@@ -0,0 +1,4 @@
+def test_custom_dataset():
+    # There is not really anything specific, we want to test here, so we just run everything and check that there are
+    # no errors.
+    import examples.custom_dataset  # noqa
