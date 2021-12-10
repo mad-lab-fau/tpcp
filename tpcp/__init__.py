1597c76cb01e6f085352f3d767a634dd11ccf1e4
Arne Küderle <a.kuederle@gmail.com>
2021-12-10 17:38:33 +0100

    Fully removed parameter for now to get back to a usable state

--- tpcp/__init__.py
+++ tpcp/__init__.py
@@ -3,16 +3,6 @@ from tpcp._algorithm import Algorithm, BaseTpcpObject, OptimizableAlgorithm
 from tpcp._algorithm_utils import get_action_params, get_results, make_action_safe, make_optimize_safe
 from tpcp._base import CloneFactory, cf, clone, get_param_names, BaseFactory
 from tpcp._dataset import Dataset
-from tpcp._parameter import (
-    hyper_parameter,
-    hyperpara,
-    optimizable_parameter,
-    optipara,
-    para,
-    parameter,
-    pure_parameter,
-    purepara,
-)
 from tpcp._pipeline import OptimizablePipeline, Pipeline
 
 __version__ = "0.3.1"
