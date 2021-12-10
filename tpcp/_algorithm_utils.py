f29ad32eeefccd77042925fa8d41382341b4b773
Arne Küderle <a.kuederle@gmail.com>
2021-12-10 17:30:23 +0100

    Reenabled function name check

--- tpcp/_algorithm_utils.py
+++ tpcp/_algorithm_utils.py
@@ -185,15 +185,14 @@ def make_action_safe(action_method: Callable):
 
     @wraps(action_method)
     def safe_wrapped(self: Algorithm, *args, **kwargs):
-        # TODO: Move this check to class level
-        # if action_method.__name__ not in get_action_methods_names(self):
-        #     raise ValueError(
-        #         "The `make_action_safe` decorator can only be applied to the action methods "
-        #         f"({get_action_methods_names(self)} for {type(self)}) of an algorithm or methods. "
-        #         f"To register an action method add the following to the class definition of {type(self)}:\n\n"
-        #         f"`    _action_methods = ({action_method.__name__},)`\n\n"
-        #         "Or append it to the tuple, if it already exists."
-        #     )
+        if action_method.__name__ not in get_action_methods_names(self):
+            raise ValueError(
+                "The `make_action_safe` decorator can only be applied to the action methods "
+                f"({get_action_methods_names(self)} for {type(self)}) of an algorithm or methods. "
+                f"To register an action method add the following to the class definition of {type(self)}:\n\n"
+                f"`    _action_methods = ({action_method.__name__},)`\n\n"
+                "Or append it to the tuple, if it already exists."
+            )
         return _check_safe_run(self, action_method, *args, **kwargs)
 
     setattr(safe_wrapped, ACTION_METHOD_INDICATOR, True)
@@ -279,12 +278,11 @@ def make_optimize_safe(self_optimize_method: Callable):
 
     @wraps(self_optimize_method)
     def safe_wrapped(self: Union[OptimizablePipeline, OptimizableAlgorithm], *args, **kwargs):
-        # TODO: Move this check to class level
-        # if self_optimize_method.__name__ != "self_optimize":
-        #     raise ValueError(
-        #         "The `safe_optimize` decorator is only meant for the `self_optimize` method, but you applied it to "
-        #         f"the `{self_optimize_method.__name__}` method"
-        #     )
+        if self_optimize_method.__name__ != "self_optimize":
+            raise ValueError(
+                "The `safe_optimize` decorator is only meant for the `self_optimize` method, but you applied it to "
+                f"the `{self_optimize_method.__name__}` method"
+            )
         return _check_safe_optimize(self, self_optimize_method, *args, **kwargs)
 
     setattr(safe_wrapped, OPTIMIZE_METHOD_INDICATOR, True)
