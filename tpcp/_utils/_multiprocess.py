97bf1c65c1e0bc992978abc6a9a05d9a514aebfb
Arne Küderle <a.kuederle@gmail.com>
2021-12-20 18:21:15 +0100

    Implemented multiprocessing using a custom backend

--- tpcp/_utils/_multiprocess.py
+++ tpcp/_utils/_multiprocess.py
@@ -1,6 +1,9 @@
 """Some helper to handle mutliprocess progressbars."""
+from concurrent.futures import Future
+from typing import Optional
 
 import joblib
+from joblib.externals.loky import get_reusable_executor
 
 
 class TqdmParallel(joblib.Parallel):
@@ -49,3 +52,21 @@ class TqdmParallel(joblib.Parallel):
             self.pbar.n = self.n_completed_tasks
             self.pbar.refresh()
         super().print_progress()
+
+
+class CustomLokyPool:
+    def __init__(self, n_jobs: int, timeout: Optional[float] = None):
+        self.pool = None
+        if n_jobs != 1:
+            self.pool = get_reusable_executor(max_workers=n_jobs, timeout=timeout)
+
+    def submit(self, task, callback):
+        if self.pool:
+            future = self.pool.submit(task())
+            future.add_done_callback(callback)
+        else:
+            fn, args, kwargs = task()
+            result = fn(*args, **kwargs)
+            result_fu = Future()
+            result_fu.set_result(result)
+            callback(result_fu)
--- tpcp/optimize/_optuna_optimize.py
+++ tpcp/optimize/_optuna_optimize.py
@@ -1,11 +1,14 @@
+from concurrent.futures import Future
 from typing import TypeVar, Any, Callable, Union, Optional, Dict
 
 import numpy as np
+from joblib import delayed
 from optuna import Trial, Study, create_study, samplers
 from optuna.trial import TrialState
 
 from tpcp import Dataset, Pipeline, clone
 from tpcp._optimize import BaseOptimize
+from tpcp._utils._multiprocess import CustomLokyPool
 from tpcp._utils._score import _SCORE_CALLABLE, _ERROR_SCORE_TYPE
 from tpcp._utils._score_optuna import _score
 from tpcp.validate import Scorer
@@ -23,6 +26,7 @@ class OptunaSearch(BaseOptimize):
         scoring: Optional[Union[_SCORE_CALLABLE, Scorer]] = None,
         study: Optional[Study] = None,
         n_trials: int = 10,
+        n_jobs: Optional[int] = None,
         return_optimized: Union[bool, str] = True,
         error_score: _ERROR_SCORE_TYPE = np.nan,
     ) -> None:
@@ -31,6 +35,7 @@ class OptunaSearch(BaseOptimize):
         self.scoring = scoring
         self.study = study
         self.n_trials = n_trials
+        self.n_jobs = n_jobs
         #  TODO: Replace return optimized with rank function or similar
         self.return_optimized = return_optimized
         self.error_score = error_score
@@ -51,28 +56,36 @@ class OptunaSearch(BaseOptimize):
         else:
             transform_score = lambda x: x
 
-        # TODO: Check if we can use proper mutliprocessing here
-        for _ in range(self.n_trials):
-            trial = self.study_.ask()
-            params = self.search_space_creator(trial)
-            p = clone(self.pipeline).set_params(**params)
-            result = _score(
-                pipeline=p,
-                dataset=self.dataset,
-                scorer=scoring,
-                parameters=params,
-                error_score=self.error_score,
-                trial=trial,
-                transform_score=transform_score,
-                return_parameters=True,
-                return_data_labels=True,
-                return_times=True
-            )
-            state = result["state"]
-            if state == TrialState.COMPLETE:
-                self.study_.tell(trial, transform_score(result["scores"]))
-            else:
-                self.study_.tell(trial, None, state=state)
+        pool = CustomLokyPool(n_jobs=self.n_jobs)
+        results = []
+        with pool:
+            for _ in range(self.n_trials):
+                trial = self.study_.ask()
+                params = self.search_space_creator(trial)
+                p = clone(self.pipeline).set_params(**params)
+                task = delayed(_score)(
+                    pipeline=p,
+                    dataset=self.dataset,
+                    scorer=scoring,
+                    parameters=params,
+                    error_score=self.error_score,
+                    trial=trial,
+                    transform_score=transform_score,
+                    return_parameters=True,
+                    return_data_labels=True,
+                    return_times=True,
+                )
+
+                def callback(result: Future):
+                    result = result.result()
+                    state = result["state"]
+                    if state == TrialState.COMPLETE:
+                        self.study_.tell(trial, transform_score(result["scores"]))
+                    else:
+                        self.study_.tell(trial, None, state=state)
+                    results.append(result)
+
+                pool.submit(task, callback)
 
         self.best_params_ = self.study_.best_params
         if self.return_optimized:
