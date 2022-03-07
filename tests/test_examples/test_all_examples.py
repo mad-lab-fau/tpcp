e6216a48286e654770ff62f8c8b503ace7900871
Arne Küderle <a.kuederle@gmail.com>
2022-03-07 16:58:23 +0100

    Added simnple tests for new examples

--- tests/test_examples/test_all_examples.py
+++ tests/test_examples/test_all_examples.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+from numpy.testing import assert_array_equal
+
 
 def test_custom_dataset():
     # There is not really anything specific, we want to test here, so we just run everything and check that there are
@@ -20,3 +22,10 @@ def test_real_life_dataset():
     subset.labeled_r_peaks_
     subset.pvc_positions_
     subset.r_peak_positions_
+
+
+def test_qrs_algorithm():
+    from examples.algorithms._01_algorithms_qrs_detection import algorithm
+
+    assert algorithm.min_r_peak_height_over_baseline == 1.1639645141095776
+    assert_array_equal(algorithm.r_peak_positions_[:3], [16, 284, 562])
