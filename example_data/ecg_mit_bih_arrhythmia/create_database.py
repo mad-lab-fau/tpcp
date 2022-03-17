0fd4716e0911ddc8d5b8add089a17c86cf9f0966
Arne Küderle <a.kuederle@gmail.com>
2022-03-17 13:02:16 +0100

    Fixed linting issues

--- example_data/ecg_mit_bih_arrhythmia/create_database.py
+++ example_data/ecg_mit_bih_arrhythmia/create_database.py
@@ -4,26 +4,31 @@ This script downloads a subset of the MIT-BIH Arrhythmia Database.
 
 Note that rerunning might change the pickle files.
 However, it is very likely that these changes are only caused by small differences in the compression.
-These changes should not be commited to git again.
+These changes should not be committed to git again.
 """
 
 from pathlib import Path
 
 import numpy as np
 import pandas as pd
-import wfdb
+import wfdb  # noqa: import-error
 
 selected_participants = ["100", "102", "104", "105", "106", "108", "114", "116", "119", "121", "123", "200"]
 
 out_dir = Path(__file__).parent / "data"
 
 for p in selected_participants:
-    record = wfdb.rdsamp(p, pn_dir='mitdb')
+    record = wfdb.rdsamp(p, pn_dir="mitdb")
     all_data = pd.DataFrame({"ecg": record[0][:, 0]})
-    anno = wfdb.rdann(p, 'atr', pn_dir='mitdb')
-    all_r = np.unique(anno.sample[np.in1d(anno.symbol,
-                                          ['N', 'L', 'R', 'B', 'A', 'a', 'J', 'S', 'V', 'r', 'F', 'e', 'j', 'n', 'E',
-                                           '/', 'f', 'Q', '?'])])
+    anno = wfdb.rdann(p, "atr", pn_dir="mitdb")
+    all_r = np.unique(
+        anno.sample[
+            np.in1d(
+                anno.symbol,
+                ["N", "L", "R", "B", "A", "a", "J", "S", "V", "r", "F", "e", "j", "n", "E", "/", "f", "Q", "?"],
+            )
+        ]
+    )
     pvc = np.unique(anno.sample[np.in1d(anno.symbol, ["V"])])
     all_r = pd.DataFrame({"R": all_r})
     pvc = pd.DataFrame({"PVC": pvc})
