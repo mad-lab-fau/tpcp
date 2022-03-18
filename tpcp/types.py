aded3d796f239e7e6987569dbac5e5c693c83a2d
Arne Küderle <a.kuederle@gmail.com>
2022-03-18 20:20:25 +0100

    New public module with all types that users might need

---
+++ tpcp/types.py
@@ -0,0 +1,12 @@
+"""Type only classes that should be used when trying to type tpcp derived classes.
+
+Note, this module only imports them, but they are defined close to places where they are used.
+"""
+
+
+from tpcp._algorithm import Algorithm_
+from tpcp._dataset import Dataset_
+from tpcp._pipeline import OptimizablePipeline_, Pipeline_
+from tpcp._utils._score import ScoreCallable
+
+__all__ = ["Pipeline_", "OptimizablePipeline_", "Dataset_", "Algorithm_", "ScoreCallable"]
