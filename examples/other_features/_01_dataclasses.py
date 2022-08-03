6a902b0460ebc3a3fbf99acf84ddd74528f147ad
Arne Küderle <a.kuederle@gmail.com>
2022-08-03 11:59:32 +0200

    Updated dataclasses example

--- examples/other_features/_01_dataclasses.py
+++ examples/other_features/_01_dataclasses.py
@@ -1,10 +1,10 @@
 """
 
 When using `tpcp` you have to write a lot of classes with a lot of parameters.
-For each class you need to repeat all paraemter names up to 3 times, even before writing any documentation.
+For each class you need to repeat all parameter names up to 3 times, even before writing any documentation.
 
 Below you can see the relevant part of the `QRSDetection` algorithm we implemented in another example.
-Eventhough it has only 3 parameters, it requires over 20 lines of code to define the basic initialization.
+Even though it has only 3 parameters, it requires over 20 lines of code to define the basic initialization.
 """
 
 import pandas as pd
@@ -37,15 +37,21 @@ class QRSDetector(Algorithm):
         self.high_pass_filter_cutoff_hz = high_pass_filter_cutoff_hz
 
 
+from dataclasses import dataclass, field
+
 # %%
 # Luckily, Python has a built-in solution for that, called `dataclasses`.
 # With that, we can write the class above much more compact.
-from dataclasses import dataclass, field
+#
+# The only downside is that the annotation of result fields and constants is a little more verbose, and you **need** to
+# make sure that these parameters are excluded from the init.
+# Otherwise, tpcp will explode ;)
+from typing import ClassVar
 
 
 @dataclass
-class QRSDetector:
-    _action_methods = "detect"
+class QRSDetector(Algorithm):
+    _action_methods: ClassVar[str] = "detect"
 
     # Input Parameters
     high_pass_filter_cutoff_hz: Parameter[float] = 200.0
@@ -53,10 +59,12 @@ class QRSDetector:
     min_r_peak_height_over_baseline: Parameter[float] = 0.5
 
     # Results
+    # We need to add the special field annotation, to exclude the parameter from the init
     r_peak_positions_: pd.Series = field(init=False)
 
     # Some internal constants
-    _HIGH_PASS_FILTER_ORDER: int = field(default=4, init=False)
+    # Using the ClassVar annotation, will mark this value as a constant and dataclasses will ignore it.
+    _HIGH_PASS_FILTER_ORDER: ClassVar[int] = 4
 
 
 # %%
@@ -64,44 +72,56 @@ class QRSDetector:
 QRSDetector(high_pass_filter_cutoff_hz=4, max_heart_rate_bpm=200, min_r_peak_height_over_baseline=1)
 
 # %%
-# Unfortunately, the way `dataclasses` work, interferes with some fundamental checks runtime checks we run on
-# our classes in tpcp.
-# If we want to make this class above a child-class of Algorithm (or any other tpcp class), we have to pass the
-# `dataclass` parameter at class initialisation.
-# Before, you ask... Yes this is valid Python!
-# The parameter will make sure that we change the way certain checks are performed under the assumption,
-# the class will be wrapped in a dataclass decorator.
-#
-# .. warning:: Don't pass the `dataclass` parameter, if you are not using the `dataclass` decorator as well!
-#              This will disable some internal checks, which might resurface as hard to debug errors later on.
-@dataclass
-class QRSDetector(Algorithm, dataclass=True):
-    _action_methods = "detect"
-
-    # Input Parameters
-    high_pass_filter_cutoff_hz: Parameter[float]
-    max_heart_rate_bpm: Parameter[float]
-    min_r_peak_height_over_baseline: Parameter[float]
-
-    # Results
-    r_peak_positions_: pd.Series = field(init=False)
-
-    # Some internal constants
-    _HIGH_PASS_FILTER_ORDER: int = field(default=4, init=False)
-
-
-# %%
 # Inheritance
 # -----------
 # Creating child classes of `dataclasses` is also simple.
 # Instead of repeating all parameters, you just need to specify the new once.
-# However, you need to make sure that you also apply the `dataclass` decorator to the child class and provide the
-# `dataclass` parameter!
+# However, you need to make sure that you also apply the `dataclass` decorator to the child class!
+#
+# But, be aware that you can not subclass a class that is not a `dataclass` and also has a `__init__` method!
+# For example, you can not subclass :class:`~tpcp.optimize.GridSearch` with a dataclass, as it already defines its own
+# `__init__`.
+# In this case you need to use a regular class and manually repeat all parent parameters
+# (and call `super().__init__()`).
 @dataclass
-class ModifiedQRSDetector(QRSDetector, dataclass=True):
+class ModifiedQRSDetector(QRSDetector):
     new_parameter: Parameter[float] = 3
 
 
 ModifiedQRSDetector(
     high_pass_filter_cutoff_hz=4, max_heart_rate_bpm=200, min_r_peak_height_over_baseline=1, new_parameter=3
 )
+
+
+# %%
+# Mutable Defaults
+# ----------------
+# In `tpcp` we usually deal with the issue of mutable defaults by using the :class:`~tpcp.CloneFactory` (
+# :func:`~tpcp.cf`).
+# However, when using dataclasses, we can use the (more elegant) `field` annotation to define mutable defaults.
+
+
+@dataclass
+class FilterAlgorithm(Algorithm):
+    _action_methods = "filter"
+
+    # Input Parameters
+    cutoff_hz: Parameter[float] = 2
+    order: Parameter[int] = 5
+
+    # Results
+    filtered_signal_: pd.Series = field(init=False)
+
+
+@dataclass
+class HigherLevelFiler(QRSDetector):
+    filter_algorithm: Parameter[FilterAlgorithm] = field(default_factory=lambda: FilterAlgorithm(3, 2))
+
+
+# %%
+# We can see that each instance will get a copy of the default value.
+v1 = HigherLevelFiler()
+v2 = HigherLevelFiler()
+
+nested_object_is_different = v1.filter_algorithm is not v2.filter_algorithm
+nested_object_is_different
