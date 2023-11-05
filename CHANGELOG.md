77e1679cd137df2c05b62a24b496c23721f0de37
Arne Küderle <a.kuederle@gmail.com>
2023-11-05 20:33:22 +0100

    Now the modules are properly registerd

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/) (+ the Migration Guide),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.26.2] - 2023-11-05
+
+## Fixed
+
+- Now actually fixed the pytest registration of the testing modules.
+
 ## [0.26.1] - 2023-11-05
 
 ## Fixed
--- tpcp/testing/__init__.py
+++ tpcp/testing/__init__.py
@@ -1,10 +1,10 @@
 """Helper for testing of algorithms and pipelines implemented in tpcp."""
 import pytest
 
-from tpcp.testing._algorithm_test_mixin import TestAlgorithmMixin
-from tpcp.testing._regression_utils import PyTestSnapshotTest
-
 pytest.register_assert_rewrite("tpcp.testing._algorithm_test_mixin")
 pytest.register_assert_rewrite("tpcp.testing._regression_utils")
+
+from tpcp.testing._algorithm_test_mixin import TestAlgorithmMixin  # noqa: E402
+from tpcp.testing._regression_utils import PyTestSnapshotTest  # noqa: E402
 
 __all__ = ["TestAlgorithmMixin", "PyTestSnapshotTest"]
