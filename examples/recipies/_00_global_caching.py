59c0f9042b6929b81c0426045e1e75e1a5acce90
Arne Küderle <a.kuederle@gmail.com>
2023-12-01 12:41:25 +0100

    Fixed implementation and example

---
+++ examples/recipies/_00_global_caching.py
@@ -0,0 +1,33 @@
+from joblib import Memory
+
+from tpcp.caching import global_disk_cache
+from examples.algorithms.algorithms_qrs_detection_final import QRSDetector
+
+
+global_disk_cache(Memory("./.cache", verbose=10))(QRSDetector)
+
+# %%
+
+from pathlib import Path
+
+from examples.datasets.datasets_final_ecg import ECGExampleData
+
+# Loading the data
+try:
+    HERE = Path(__file__).parent
+except NameError:
+    HERE = Path().resolve()
+data_path = HERE.parent.parent / "example_data/ecg_mit_bih_arrhythmia/data"
+example_data = ECGExampleData(data_path)
+ecg_data = example_data[0].data["ecg"]
+
+# Initialize the algorithm
+algorithm = QRSDetector()
+algorithm = algorithm.detect(ecg_data, example_data.sampling_rate_hz)
+
+# %%
+
+algorithm = QRSDetector()
+algorithm = algorithm.detect(ecg_data, example_data.sampling_rate_hz)
+
+
