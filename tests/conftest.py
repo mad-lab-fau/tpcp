8c138a9578cb580ea5b8e2e922fe5029fa38fd2f
Annika Mücke <annika-muecke@outlook.de>
2024-06-26 11:12:01 +0200

    new attempt to fic conftest.py after merging

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,36 +1,8 @@
 from typing import Any
 
-import pytest
-
 from tpcp import BaseTpcpObject
 from tpcp._base import _BaseTpcpObject
 
 
 def _get_params_without_nested_class(instance: BaseTpcpObject) -> dict[str, Any]:
     return {k: v for k, v in instance.get_params().items() if not isinstance(v, _BaseTpcpObject)}
-
-
-@pytest.fixture()
-def snapshot(request):
-    from tpcp.testing import PyTestSnapshotTest
-
-    with PyTestSnapshotTest(request) as snapshot_test:
-        yield snapshot_test
-
-
-def pytest_addoption(parser):
-    group = parser.getgroup("tpcp_snapshots")
-    group.addoption(
-        "--snapshot-update",
-        action="store_true",
-        default=False,
-        dest="snapshot_update",
-        help="Update the snapshots.",
-    )
-    group.addoption(
-        "--snapshot-only-check",
-        action="store_true",
-        default=False,
-        dest="snapshot_only_check",
-        help="Run as normal, but fail if a snapshot file is not found. This is usefull for CI runs.",
-    )
