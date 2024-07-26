393c6e1b72b2a5986a79fb29df6036019fd2046b
Arne Küderle <a.kuederle@gmail.com>
2024-07-26 18:44:45 +0200

    Formatting

--- tpcp/validate/__init__.py
+++ tpcp/validate/__init__.py
@@ -1,6 +1,15 @@
 """Module for all helper methods to evaluate algorithms."""
 from tpcp.validate._cross_val_helper import DatasetSplitter
-from tpcp.validate._scorer import Aggregator, Scorer, mean_agg, no_agg, FloatAggregator
+from tpcp.validate._scorer import Aggregator, FloatAggregator, Scorer, mean_agg, no_agg
 from tpcp.validate._validate import cross_validate, validate
 
-__all__ = ["Scorer", "no_agg", "Aggregator", "mean_agg", "cross_validate", "validate", "DatasetSplitter", "FloatAggregator"]
+__all__ = [
+    "Scorer",
+    "no_agg",
+    "Aggregator",
+    "mean_agg",
+    "cross_validate",
+    "validate",
+    "DatasetSplitter",
+    "FloatAggregator",
+]
