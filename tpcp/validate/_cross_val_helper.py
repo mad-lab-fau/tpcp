ce6a6c92bfae88f1258b1c90e2b0e0db59ddf02a
Arne Küderle <a.kuederle@gmail.com>
2025-07-24 12:14:06 +0200

    Datasetsplitter now autoselect suitable KFold splitter based on inputs. Closes #121

--- tpcp/validate/_cross_val_helper.py
+++ tpcp/validate/_cross_val_helper.py
@@ -1,7 +1,9 @@
+import numbers
+import warnings
 from collections.abc import Iterator
 from typing import Optional, Union
 
-from sklearn.model_selection import BaseCrossValidator, check_cv
+from sklearn.model_selection import BaseCrossValidator, GroupKFold, StratifiedGroupKFold, StratifiedKFold, check_cv
 
 from tpcp import BaseTpcpObject, Dataset
 
@@ -12,11 +14,11 @@ class DatasetSplitter(BaseTpcpObject):
     This wrapper can be used instead of a sklearn-style splitter with all methods that support a ``cv`` parameter.
     Whenever you want to do complicated cv-logic (like grouping or stratification's), this wrapper is the way to go.
 
-    .. warning:: We don't validate if the selected ``base_splitter`` does anything useful with the provided
-        ``groupby`` and ``stratify`` information.
-        This wrapper just ensures, that the information is correctly extracted from the dataset and passed to the
-        ``split`` method of the ``base_splitter``.
-        So if you are using a normal ``KFold`` splitter, the ``groupby`` and ``stratify`` arguments will have no effect.
+    You can either select your own base splitter, or we will select from KFold, StratifiedKFold, GroupKFold, or
+    StratifiedGroupKFold, depending on the provided ``groupby`` and ``stratify`` parameters.
+
+    .. warning:: If you use a custom splitter, that does not support grouping or stratification, these parameters might
+        be silently ignored.
 
     Parameters
     ----------
@@ -38,7 +40,11 @@ class DatasetSplitter(BaseTpcpObject):
         This will passed to the base splitter as the ``y`` parameter, acting as "mock" target labels, as sklearn only
         support stratification on classification outcome targets.
         It is up to the base splitter to decide what to do with the generated labels.
-
+    ignore_potentially_invalid_splitter_warning
+        We are trying to detect if the provided splitter supports grouping and stratification.
+        If they are not supported, but you provided groupby or stratify columns, we will warn you.
+        Note, that this warning is not a perfect check, as it is not possible to detect all cases.
+        If you know what you are doing, and you want to disable this warning, set this parameter to True.
     """
 
     def __init__(
@@ -47,13 +53,57 @@ class DatasetSplitter(BaseTpcpObject):
         *,
         groupby: Optional[Union[str, list[str]]] = None,
         stratify: Optional[Union[str, list[str]]] = None,
+        ignore_potentially_invalid_splitter_warning: bool = False,
     ):
         self.base_splitter = base_splitter
         self.stratify = stratify
         self.groupby = groupby
+        self.ignore_potentially_invalid_splitter_warning = ignore_potentially_invalid_splitter_warning
 
     def _get_splitter(self):
-        return check_cv(self.base_splitter, y=None, classifier=True)
+        cv = self.base_splitter
+        cv = 5 if cv is None else cv
+        if isinstance(cv, numbers.Integral):
+            if self.groupby is not None and self.stratify is not None:
+                cv = StratifiedGroupKFold(n_splits=cv)
+            elif self.groupby is not None:
+                cv = GroupKFold(n_splits=cv)
+            elif self.stratify is not None:
+                cv = StratifiedKFold(n_splits=cv)
+        cv = check_cv(cv, y=None, classifier=True)
+
+        if self.ignore_potentially_invalid_splitter_warning:
+            return cv
+
+        # The checks below might be redundant, but it makes the code structure easier to follow.
+        msg = None
+        if self.groupby and "Group" not in self.base_splitter.__class__.__name__:
+            msg = (
+                "You specified groupby columns for the splitter, but it looks like you did not selected any of the "
+                "typical sklearn splitters that do support grouping. "
+                "Splitters that don't support grouping will silently ignore the grouping information.",
+            )
+        if self.stratify and "Stratified" not in self.base_splitter.__class__.__name__:
+            msg = (
+                "You specified stratify columns for the splitter, but it looks like you did not selected any of the "
+                "typical sklearn splitters that do support stratification. "
+                "Splitters that don't support stratification will silently ignore the stratification information.",
+            )
+        if msg is not None:
+            warnings.warn(
+                (
+                    f"{msg}"
+                    "\nTo fix this issue pass a splitter that supports the required functionality as `base_splitter`."
+                    "For a list of available splitters see "
+                    "https://scikit-learn.org/stable/api/sklearn.model_selection.html "
+                    "\nIf you provided a custom splitter, and you know what you are doing, you can disable this "
+                    "warning, by setting the `ignore_potentially_invalid_splitter_warning=False` when creating the "
+                    "DatasetSplitter object."
+                ),
+                UserWarning,
+                stacklevel=2,
+            )
+        return cv
 
     def _get_labels(self, dataset: Dataset, labels: Union[None, str, list[str]]):
         if labels:
