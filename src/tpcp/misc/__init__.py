4f84058c3727c37698968ddeec2131b8ba0e1cd6
Arne Küderle <a.kuederle@gmail.com>
2026-06-15 17:02:56 +0200

    Add warning and error context helper

--- src/tpcp/misc/__init__.py
+++ src/tpcp/misc/__init__.py
@@ -3,6 +3,7 @@
 from tpcp._hash import custom_hash
 from tpcp.misc._class_utils import classproperty, set_defaults
 from tpcp.misc._typed_iterator import BaseTypedIterator, TypedIterator, TypedIteratorResultTuple
+from tpcp.misc._warning_error_context import warning_error_context
 
 __all__ = [
     "BaseTypedIterator",
@@ -11,4 +12,5 @@ __all__ = [
     "classproperty",
     "custom_hash",
     "set_defaults",
+    "warning_error_context",
 ]
